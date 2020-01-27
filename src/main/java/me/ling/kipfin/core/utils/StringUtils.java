62419925a1a453c20dabf51a3d55f2913027878f
Diego <msnimpu@gmail.com>
2020-01-27 10:59:02 +0300

    Добавлемы методы генерации случайных строк

--- src/main/java/me/ling/kipfin/core/utils/StringUtils.java
+++ src/main/java/me/ling/kipfin/core/utils/StringUtils.java
@@ -19,6 +19,8 @@
 
 package me.ling.kipfin.core.utils;
 
+import org.jetbrains.annotations.NotNull;
+
 /**
  * Утилиты строк
  */
@@ -26,19 +28,53 @@ public class StringUtils {
 
     /**
      * Удаляет все лишиние пробелы
+     *
      * @param s - входная строка
-     * @return  - выходная строка
+     * @return - выходная строка
      */
-    public static String removeAllSpaces(String s){
+    @NotNull
+    public static String removeAllSpaces(@NotNull String s) {
         return s.replaceAll("^\\s+|\\s+$", "").replaceAll("\\s+", " ");
     }
 
     /**
      * Возвращает строку с заглавной буквы
+     *
      * @param s - исходная строка
      * @return - строка с заглавной буквы
      */
-    public static String title(String s){
+    @NotNull
+    public static String title(@NotNull String s) {
         return s.substring(0, 1).toUpperCase() + s.substring(1);
+    }
+
+    /**
+     * Создает и возвращает случайную строку длиной n из символов source
+     *
+     * @param n      - длина
+     * @param source - алфавит символов
+     * @return - случайная сгенерированная стрка
+     */
+    @NotNull
+    public static String getRandomString(int n, String source) {
+        StringBuilder sb = new StringBuilder(n);
+
+        for (int i = 0; i < n; i++) {
+            int index = (int) (source.length() * Math.random());
+            sb.append(source.charAt(index));
+        }
+        return sb.toString();
+    }
+
+    /**
+     * Возвращает случайную строку размера n
+     *
+     * @param n - длина случайной строки
+     * @return - случайная строка
+     */
+    @NotNull
+    public static String getAlphaNumericString(int n) {
+        return StringUtils.getRandomString(n,
+                "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvxyz");
     }
 }
