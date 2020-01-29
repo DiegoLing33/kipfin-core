8289b87c94919f187ffea524a3d91250a4c5b61c
diego <masnimpu@gmail.com>
2020-01-29 20:21:11 +0300

    Добавлены новые методы поиска по списку

--- src/main/java/me/ling/kipfin/core/utils/ListUtils.java
+++ src/main/java/me/ling/kipfin/core/utils/ListUtils.java
@@ -2,7 +2,10 @@ package me.ling.kipfin.core.utils;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.lang.ref.Reference;
 import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 /**
  * Утилиты списков
@@ -21,4 +24,83 @@ public class ListUtils {
         return list.get(IntUtils.getRandomInteger(list.size() - 1));
     }
 
+    /**
+     * Проверяет наличие по строгому соотвествию
+     *
+     * @param list  - список
+     * @param value - значение для теста
+     * @param <T>   - тип значения для теста
+     * @return - результат тестирования
+     */
+    public static <T> boolean contains(@NotNull List<T> list, T value) {
+        return list.contains(value);
+    }
+
+    /**
+     * Проверяет наличие по НЕстрогому соотвествию
+     * <p>
+     * Данное тестирование работает по принципу:
+     * ```
+     * var x = StringUtils.removeAllSpaces(a.toLoverCase());
+     * var y = StringUtils.removeAllSpaces(b.toLoverCase());
+     * <p>
+     * var result = a.contains(b);
+     * ```
+     *
+     * @param list  - лист
+     * @param value - значение
+     * @return - результат тестирования
+     */
+    public static boolean containsLike(@NotNull List<String> list, String value) {
+        return ListUtils.contains(list, (Predicate<String>) s -> StringUtils.removeAllSpaces(s.toLowerCase())
+                .contains(StringUtils.removeAllSpaces(value.toLowerCase())));
+    }
+
+    /**
+     * Выполняет поиск по листу
+     *
+     * @param list      - лист
+     * @param predicate - предикат
+     * @param <T>       - тип листа
+     * @return - результат
+     */
+    public static <T> boolean contains(@NotNull List<T> list, Predicate<T> predicate) {
+        return list.stream().anyMatch(predicate);
+    }
+
+    /**
+     * Выполняет поиск по листу с помощью полей объекта
+     *
+     * @param list      - список
+     * @param reference - ссылка
+     * @param test      - тестируемое значение
+     * @param <T>       - тип тестируемого объекта
+     * @param <P>       - тип тестируемого значение
+     * @return - результат тестирования
+     */
+    public static <T, P> boolean containsWhere(@NotNull List<T> list, Function<T, P> reference, P test) {
+        return ListUtils.contains(list, (Predicate<T>) s -> reference.apply(s).equals(test));
+    }
+
+    /**
+     * Выполняет поиск по листу с помощью полей объекта. Поиск производится по "похожим элементам"
+     * <p>
+     * Данное тестирование работает по принципу:
+     * ```
+     * var x = StringUtils.removeAllSpaces(a.toLoverCase());
+     * var y = StringUtils.removeAllSpaces(b.toLoverCase());
+     * <p>
+     * var result = a.contains(b);
+     * ```
+     *
+     * @param list      - список
+     * @param reference - ссылка на поле
+     * @param like      - тестируемое значение
+     * @param <T>       - тип объекта
+     * @return - результат тестирования
+     */
+    public static <T> boolean containsLike(@NotNull List<T> list, Function<T, String> reference, String like) {
+        return ListUtils.contains(list, (Predicate<T>) s -> StringUtils.removeAllSpaces(reference.apply(s))
+                .toLowerCase().contains(StringUtils.removeAllSpaces(like.toLowerCase())));
+    }
 }
