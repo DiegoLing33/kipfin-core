8f31d9e169094cd43193014e8bf8c563d7924bb0
Diego <msnimpu@gmail.com>
2020-02-14 11:27:36 +0300

    Дополнение DateUtils - работа со временем

--- src/main/java/me/ling/kipfin/core/utils/DateUtils.java
+++ src/main/java/me/ling/kipfin/core/utils/DateUtils.java
@@ -20,8 +20,11 @@
 package me.ling.kipfin.core.utils;
 
 import me.ling.kipfin.exceptions.WrongDateException;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
@@ -30,14 +33,9 @@ import java.time.format.DateTimeParseException;
  */
 public final class DateUtils {
 
-    /**
-     * Дни недели
-     */
+    public static String TIME_FORMAT = "HH:mm:ss";
+    public static String DATE_FORMAT = "dd.MM.yyyy";
     public static final String[] weekDaysNames = {"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"};
-
-    /**
-     * Паттерн локального формата даты
-     */
     public static final String localDatePattern = "^[0-9]{2}\\.[0-9]{2}\\.[0-9]{4}$";
 
     /**
@@ -46,8 +44,9 @@ public final class DateUtils {
      * @param date - дата
      * @return - строка даты
      */
-    public static String toLocalDateString(LocalDate date) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    @NotNull
+    public static String toLocalDateString(@NotNull LocalDate date) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
         return date.format(formatter);
     }
 
@@ -58,7 +57,7 @@ public final class DateUtils {
      * @return - объект даты
      */
     public static LocalDate fromLocalDateString(String localDateString) throws DateTimeParseException {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
         return LocalDate.parse(localDateString, formatter);
     }
 
@@ -68,7 +67,9 @@ public final class DateUtils {
      * @param localDateString - исходная стркоа в РУС формате
      * @return - результат тестирования
      */
-    public static Boolean isStringDateInLocalFormat(String localDateString) {
+    @NotNull
+    @Contract(pure = true)
+    public static Boolean isStringDateInLocalFormat(@NotNull String localDateString) {
         return localDateString.matches(DateUtils.localDatePattern);
     }
 
@@ -85,12 +86,32 @@ public final class DateUtils {
     }
 
     /**
+     * Преобразует время в локальную строку
+     * @param time  - время
+     * @return  - строка в формате `DateUtils::TIME_FORMAT`
+     */
+    @NotNull
+    public static String toLocalTimeString(@NotNull LocalTime time){
+        return time.format(DateTimeFormatter.ofPattern(TIME_FORMAT));
+    }
+
+    /**
+     * Возвращает время из локальной строки времени
+     * @param localTime - локальная строка времени
+     * @return  - объект LocalTime
+     */
+    public static LocalTime getTimeFromLocalTimeString(String localTime){
+        return LocalTime.parse(localTime, DateTimeFormatter.ofPattern(TIME_FORMAT));
+    }
+
+    /**
      * Возвращает день недели
      *
      * @param date - дата
      * @return - день недели (индекс)
      */
-    public static Integer getLocalWeekDay(LocalDate date) {
+    @NotNull
+    public static Integer getLocalWeekDay(@NotNull LocalDate date) {
         return date.getDayOfWeek().getValue() - 1;
     }
 
@@ -101,7 +122,8 @@ public final class DateUtils {
      * @param currentDate - вторая дата
      * @return - количество недель между датами
      */
-    public static Integer getWeeksCount(LocalDate startDate, LocalDate currentDate) {
+    @NotNull
+    public static Integer getWeeksCount(@NotNull LocalDate startDate, @NotNull LocalDate currentDate) {
         long ms1 = startDate.toEpochDay();
         long ms2 = currentDate.toEpochDay();
         long dif = Math.abs(ms2 - ms1);
